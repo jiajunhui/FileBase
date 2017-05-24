4e4cc8b94ac6dd7d03223101c8365d7b2cb74bc5
jiajunhui <309812983@qq.com>
2017-05-24 18:17:50 +0800

    modify some
    modify some

--- FileBase/src/main/java/com/kk/taurus/filebase/engine/FileEngine.java
+++ FileBase/src/main/java/com/kk/taurus/filebase/engine/FileEngine.java
@@ -21,6 +21,8 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
 import android.util.Log;
@@ -233,7 +235,7 @@ public class FileEngine {
      * @param apkPath
      * @return
      */
-    public static Drawable getApkIcon(Context context, String apkPath) {
+    public static Drawable getApkDrawable(Context context, String apkPath) {
         PackageManager pm = context.getPackageManager();
         PackageInfo info = pm.getPackageArchiveInfo(apkPath,
                 PackageManager.GET_ACTIVITIES);
@@ -248,6 +250,38 @@ public class FileEngine {
             }
         }
         return null;
+    }
+
+    /**
+     * from apk file get apk icon bitmap.
+     * @param context
+     * @param apkPath
+     * @return
+     */
+    public static Bitmap getApkBitmap(Context context, String apkPath) {
+        Drawable drawable = getApkDrawable(context, apkPath);
+        if(drawable!=null){
+            return drawableToBitmap(drawable);
+        }
+        return null;
+    }
+
+    /**
+     * drawable to bitmap.
+     * @param drawable
+     * @return
+     */
+    public static Bitmap drawableToBitmap(Drawable drawable){
+        Bitmap bitmap = Bitmap.createBitmap(
+                drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(),
+                drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+                        : Bitmap.Config.RGB_565);
+        Canvas canvas = new Canvas(bitmap);
+        //canvas.setBitmap(bitmap);
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        drawable.draw(canvas);
+        return bitmap;
     }
 
     /**
